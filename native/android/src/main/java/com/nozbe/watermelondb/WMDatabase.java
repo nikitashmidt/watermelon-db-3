199b8cb8af01dd4c758310e8246be7c8104be304
nikitashmidt <nikitka.shmidt.01@gmail.com>
2026-01-23 17:20:39 +0600

    fix

--- native/android/src/main/java/com/nozbe/watermelondb/WMDatabase.java
+++ native/android/src/main/java/com/nozbe/watermelondb/WMDatabase.java
@@ -63,18 +63,7 @@ public class WMDatabase {
             database.enableWriteAheadLogging();
         }
         database.execSQL("PRAGMA case_sensitive_like=OFF;");
-        database.execSQL("SELECT icu_load_collation('ru', 'RUSSIAN_CI')");
-        database.execSQL("SELECT icu_load_collation('ru', 'RUSSIAN')");
-        database.execSQL("SELECT icu_load_collation('ru', 'RU')");
-
-        database.execSQL("""
-            CREATE COLLATION RUSSIAN_CI 
-            AS (CASE 
-                WHEN UPPER(?) = UPPER(?) THEN 0 
-                WHEN ? < ? THEN -1 
-                ELSE 1 
-            END)
-        """)
+        database.execSQL("PRAGMA encoding = 'UTF-8'")
         return database;
     }
 
